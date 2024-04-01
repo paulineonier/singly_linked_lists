101102a2491ef82f4c96971a2624519181c9cb54
paulineonier <pauline.onier@outlook.fr>
2024-04-01 20:52:57 +0000

    task 4

---
+++ 4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees the list list_t
+ * head: pointer to the head, star of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
+    }
+}
