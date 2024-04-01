119e607699da370d78230d226e1abfd506039d88
paulineonier <pauline.onier@outlook.fr>
2024-04-01 20:55:02 +0000

    task 4 correction

--- 4-free_list.c
+++ 4-free_list.c
@@ -3,18 +3,18 @@
 
 /**
  * free_list - frees the list list_t
- * head: pointer to the head, star of the list
+ * @head: pointer to the head, star of the list
  */
 
 void free_list(list_t *head)
 {
 	list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-        free(temp);
-    }
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
